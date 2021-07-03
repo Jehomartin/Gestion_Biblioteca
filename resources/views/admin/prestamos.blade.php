699d298eba22db3f64d1f0bc88e100a7e91889be
Jehovani Chan <jehomartin.04@gmail.com>
2021-07-03 11:21:05 -0500

    cambios en diseño
    Se realizaron algunos pequeños cambios en el diseño de la vista

--- resources/views/admin/prestamos.blade.php
+++ resources/views/admin/prestamos.blade.php
@@ -4,7 +4,7 @@
 
 <center>
   <font color="orange" face="arial black">
-    <h2>Procesando el prestamo del libro</h2>
+    <h2>PROCESANDO PRESTAMO DE LIBRO</h2>
   </font>
 </center>
 
@@ -12,26 +12,26 @@
 <!--  <font color="white" size="10">@{{saludo}}</font> -->
   <br>
   <div class="container">
-    <font color="black" face="arial black"><h5>FOLIO : @{{folioprestamo}}</h5></font>
-    <font color="black" face="arial black"><h5 v-model="fechaprestamo">FECHA PRESTAMO : @{{fechaprestamo}}</h5></font>
+    <font color="green" face="arial black"><h5>FOLIO : @{{folioprestamo}}</h5></font>
+    <font color="green" face="arial black"><h5 v-model="fechaprestamo">FECHA PRESTAMO : @{{fechaprestamo}}</h5></font>
 
     <div class="row">
       <div class="col-xs-6">
-        <div >          
-          <input type="text" class="form-control" v-model="codigo" ref="buscar" v-on:keyup.enter="getLibro()" placeholder="Ingrese el codigo del libro">
+        <div>
+          <input type="text" class="form-control" v-model="codigo" ref="buscar" v-on:keyup.enter="getLibro()" placeholder="Ingrese el codigo del libro" style="color: #000">
           <br>
-          <button class="btn btn-danger" @click="getLibro()">
+          <button class="btn btn-warning glyphicon glyphicon-ok" @click="getLibro()">
             Agregar
           </button>
         </div>
         <br>
-        <button class="btn btn-primary form-control" @click="prestar()">
+        <button class="btn btn-primary form-control glyphicon glyphicon-save" @click="prestar()">
           GUARDAR PRESTAMO
         </button>
       </div>
       <div class="col-xs-4">
         <a href="{{url('devoluciones')}}">
-          <button class="btn btn-success glyphicon glyphicon-list" style="float: right;"> Verificar Prestamos</button>
+          <button class="btn btn-danger glyphicon glyphicon-list" style="float: right;"> Verificar Prestamos</button>
         </a>
       </div>
     </div>
@@ -50,9 +50,9 @@
             <th width="9%">ACCIONES</th>
           </thead>
           <tbody class="table table-bordered">
-            <tr v-for="(p,index) in prestamos">
-              <td v-model="isbn">@{{p.isbn}}</td>
-              <td v-model="titulo">@{{p.titulo}}</td>
+            <tr v-for="(p,index) in prestamos" class="colors">
+              <td v-model="isbn"> @{{p.isbn}} </td>
+              <td v-model="titulo"> @{{p.titulo}} </td>
               <td>
                 <input type="date" class="form-control" placeholder="fecha devolucion" v-model="fechadevolucion">
               </td>
